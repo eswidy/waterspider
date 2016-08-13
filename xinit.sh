372226dc55ce528f8ae5006e9ef46e4d193c927d
root <root@localhost.localdomain>
2016-08-13 17:15:21 +0000

    add the portable env inital and modified the code display the crwal result saved text

---
+++ xinit.sh
@@ -0,0 +1,8 @@
+pip -q install scrapy 
+apt-get install -y  libmysqld-dev
+mkdir ~/dev
+cd ~/dev 
+git https://github.com/eswidy/waterspider.git
+cat /etc/issue 
+# get the mysql connector 
+# http://dev.mysql.com/downloads/connector/python/
