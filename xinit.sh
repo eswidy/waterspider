48bbfa28f3d10a0d9c4fc2d31a86389acb48d9d0
eswidy <win.acc@gmail.com>
2016-08-13 22:06:42 +0000

    add the dev username and email address

--- xinit.sh
+++ xinit.sh
@@ -1,6 +1,8 @@
 pip -q install scrapy 
 pip -q install HTMLParser
 apt-get install -y  libmysqld-dev
+git config --global user.name "eswidy"
+git config --global user.email "win.acc@gmail.com"
 mkdir ~/dev
 cd ~/dev 
 git https://github.com/eswidy/waterspider.git
