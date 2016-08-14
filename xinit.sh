c34d8f9c9e9cbf3aa07d1e59f7029054855f26af
eswidy <win.acc@gmail.com>
2016-08-14 14:46:16 +0000

    modifed the project clone problem

--- xinit.sh
+++ xinit.sh
@@ -5,7 +5,7 @@ git config --global user.name "eswidy"
 git config --global user.email "win.acc@gmail.com"
 mkdir ~/dev
 cd ~/dev 
-git https://github.com/eswidy/waterspider.git
+git clone  https://github.com/eswidy/waterspider.git
 cat /etc/issue 
 # get the mysql connector 
 # http://dev.mysql.com/downloads/connector/python/
