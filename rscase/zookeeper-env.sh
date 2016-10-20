ba651a83ba2f88b86ab2dd93ebfd99f685f84c4c
root <win.acc@gmail.com>
2016-10-20 09:58:01 +0800

    add the rs log message

---
+++ rscase/zookeeper-env.sh
@@ -0,0 +1,9 @@
+
+export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_102
+export ZOOKEEPER_HOME=/usr/hdp/current/zookeeper-server
+export ZOO_LOG_DIR=/var/log/zookeeper
+export ZOOPIDFILE=/var/run/zookeeper/zookeeper_server.pid
+export SERVER_JVMFLAGS=-Xmx1500m
+export JAVA=$JAVA_HOME/bin/java
+export CLASSPATH=$CLASSPATH:/usr/share/zookeeper/*
+
